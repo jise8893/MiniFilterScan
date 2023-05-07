6d9f010ef81dfb951542c6bd85a95ca8364ffe8a
jise8893 <60957575+jise8893@users.noreply.github.com>
2023-05-08 00:01:06 +0900

    Update scanuser.cpp

--- scanuser/scanuser.cpp
+++ scanuser/scanuser.cpp
@@ -1,5 +1,13 @@
 #include "scanuser.h"
 
+
+UCHAR MaliciousData[32] = {
+	   0x32, 0x8C, 0x50, 0x22, 0xCC, 0xC9, 0x50, 0xD0,
+	   0xBC, 0x9C, 0xE3, 0xE3, 0xF7, 0x52, 0xCC, 0x0B,
+	   0x93, 0x63, 0xAB, 0x94, 0x9D, 0x46, 0xC6, 0x3D,
+	   0xDC, 0x4C, 0x95, 0xFF, 0x2D, 0x2F, 0x26, 0x09
+};
+
 DWORD WorkerThread(WorkerContext * Context)
 {
 	HANDLE port = Context->port;
@@ -10,26 +18,40 @@ DWORD WorkerThread(WorkerContext * Context)
 	BOOL result;
 	ScannerGetMsg* msg;
 	ReplyMsg repmsg;
+	DWORD replylenth= sizeof(repmsg.header)+sizeof(repmsg.safetoOpen);
 	while (true) {
+	
 		result = GetQueuedCompletionStatus(IocpCore, &dwTrBytes, &ulKey, &pOvlp, INFINITE);
 		msg = CONTAINING_RECORD(pOvlp, ScannerGetMsg, ovlp); //ovlp에서 [ Header |  ScannerMsg  | ovlp   ] povlp에서 구조체내의 ovlp 와 맞춰서 상위 구조체를 찾는 매크로 
-		printf("Recevied message size %d\n", pOvlp->InternalHigh);
-
+		printf("Recevied message size %d\n",msg->msg.size);
 
 		for (int i = 0; i <msg->msg.size ; i++)
 		{
 			printf("%02x", msg->msg.buffer[i]);
 		}
-		repmsg.header.MessageId = msg->header.MessageId;
+		printf("\n");
+
+		repmsg.header.MessageId= msg->header.MessageId;
 		repmsg.header.Status = 0;
-		repmsg.safetoOpen = true;
-		HRESULT hr = FilterReplyMessage(port, (PFILTER_REPLY_HEADER)&repmsg, sizeof(ReplyMsg));
-		if (SUCCEEDED(hr))
+		repmsg.safetoOpen = 1;
+
+		if (memcmp(msg->msg.buffer, MaliciousData, msg->msg.size)==0)
 		{
-			printf("Replied Msg\n");
+			printf("Protected Malicous File! \n");
+		
+			repmsg.safetoOpen = 0;
 		}
 		else {
-			printf("UnReplied Msg\n");
+		
+			repmsg.safetoOpen = 1;
+		}
+		printf("Replying message, SafeToOpen: %d replymsg size %d\n", repmsg.safetoOpen,replylenth);
+
+		HRESULT hr = FilterReplyMessage(port, (PFILTER_REPLY_HEADER)&repmsg, replylenth);
+
+		if (SUCCEEDED(hr))
+		{
+			printf("Successed FilterReplyMessage\n");
 		}
 
 		memset(&msg->ovlp, 0, sizeof(OVERLAPPED));
@@ -37,8 +59,9 @@ DWORD WorkerThread(WorkerContext * Context)
 		if (hr != HRESULT_FROM_WIN32(ERROR_IO_PENDING)) {
 			msg = NULL;
 			cout << "Failed FilterGetMessage\n";
+			break;
 		}
-			
+		
 	}
 
 	return 0;
